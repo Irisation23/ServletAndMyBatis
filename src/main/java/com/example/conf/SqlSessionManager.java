3fadd03522d2f51294a8d98d17df11d2a1cf1810
bunsung92 <bunsung92@naver.com>
2023-05-03 20:59:17 +0900

    Servlet과 MyBatis 연동을 위한 초기 환경 구축 #1
    작업이력 자세한 내용은 해당 이슈 참조

---
+++ src/main/java/com/example/conf/SqlSessionManager.java
@@ -0,0 +1,27 @@
+package com.example.conf;
+
+import java.io.IOException;
+import java.io.Reader;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlSessionManager {
+    public static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        String resource = "configuration.xml";
+        Reader reader = null;
+
+        try {
+            reader = Resources.getResourceAsReader(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static SqlSessionFactory getSqlSessionFactory() {
+        return sqlSessionFactory;
+    }
+}
---
+++ src/main/webapp/dbBase/dbApp1.jsp
@@ -0,0 +1,19 @@
+<%@ page import="org.apache.ibatis.session.SqlSessionFactory" %>
+<%@ page import="com.example.conf.SqlSessionManager" %>
+<%@ page import="org.apache.ibatis.session.SqlSession" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<%
+    SqlSessionFactory sessionFactory = SqlSessionManager.getSqlSessionFactory();
+    SqlSession sqlSession = sessionFactory.openSession();
+    out.println(sqlSession);
+%>
+
+EMP<%=sqlSession.selectList("EMP") %>
+
+</body>
+</html>
